7c39c61bf030c2bbc11259309f8fd01bc7f17532
MarcosRiverosdev <118026067+MarcosRiverosdev@users.noreply.github.com>
2023-10-09 20:03:50 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 from chabella import app
-from chabella.controllers import users
-from chabella.controllers import products
-from chabella.controllers import categories
+from chabella.controllers import users, products, categories
 
 if __name__ == '__main__':
     app.run(debug=True)
